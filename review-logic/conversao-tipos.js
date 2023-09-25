3db97f9a6d98bf57e45a68823bf9bbe8c217c7d7
BianchiDev <ifpebianchi@gmail.com>
2023-09-25 09:58:07 -0300

    Review operato logic

--- review-logic/conversao-tipos.js
+++ review-logic/conversao-tipos.js
@@ -8,6 +8,12 @@ let n4 ="10";
 let n5 = "2";
 n5 = parseInt(n5);// ParseInt transforma uma Instring em um number 
 
+
+let n6 = "10.89798797989"
+n6 = parseFloat(n6);
+
+console.log(n6, typeof n6); // parseFloat converte uma string em um (number do tipo float)
+
 console.log(n1 * n2, typeof n1,  typeof n2);
 
 console.log(n1 / n2, typeof n1,  typeof n2);
@@ -23,3 +29,10 @@ console.log(n2 + n4, typeof n2,  typeof n4);// tipo iguas, quando string, concat
 
 
 console.log(n1 * n5, typeof n1,  typeof n5); // A instring foi convertida em (number do tipo inteiro) e por isso ouve uma soma ao inves de uma concatenação. 
+
+
+n8 = 11;
+
+n8 = n8.toString();// toString converte um número em String.
+
+console.log(n8 , typeof n8);
