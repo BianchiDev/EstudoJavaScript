302a750deddc0da21fea51db70297a24c8d73da0
BianchiDev <ifpebianchi@gmail.com>
2023-10-04 00:45:32 -0300

    Método de interar

---
+++ review-logic/arrays.js
@@ -0,0 +1,7 @@
+// Iteração com o sistema
+
+let arr = ["felipe", "pedro", "Iara", "kayky"];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(`${i} : Nom é ${arr[i]}`);
+}
---
+++ review-logic/object2.js
@@ -0,0 +1,15 @@
+const pessoa = {
+  nome: "maria",
+  idade: 22,
+  email: "ifpebianchi@gmail.com",
+};
+
+console.log(pessoa);
+// for (let prop in pessoa) {
+//   console.log(prop);
+//   console.log(pessoa[prop]);
+// }
+
+for (let prop in pessoa) {
+  console.log(prop + ": " + pessoa[prop]);
+}
